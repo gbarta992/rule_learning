6dc2e1023e80a6d9e7139989876d60ad42f91937
sonka1992 <sonka1992@hotmail.com>
2014-11-28 19:53:03 +0100

    Initial maven projekt

---
+++ src/main/java/hu/mi/rulelearning/RuleLearningMain.java
@@ -0,0 +1,13 @@
+package hu.mi.rulelearning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RuleLearningMain {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(RuleLearningMain.class);
+    
+    public static void main(String[] args){
+        LOGGER.debug("Hello World!");
+    }
+}
