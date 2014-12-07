34f299e2344c2a4936dd12fa58f5eba78c6919bb
sonka1992 <sonka1992@hotmail.com>
2014-12-07 16:06:04 +0100

    Pelda text fajl beolvasasra

---
+++ src/main/java/hu/mi/rulelearning/model/BaseRate.java
@@ -0,0 +1,24 @@
+package hu.mi.rulelearning.model;
+
+public class BaseRate {
+    
+    private String year;
+    private double percentage;
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+    
+}
